d5a1017b2ab933fba7f8049016e6ac113518c988
Anton Patsev <apatsev@luxoft.com>
2019-08-09 13:59:46 +0600

    requirements_install.sh deploy_and_bootstrap.sh

--- start_script.sh
+++ start_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#vagrant up
+vagrant up
 git clone https://github.com/open-io/ansible-playbook-openio-deployment.git --branch 19.04 oiosds
 mv oiosds/products/sds .
 rm -rf oiosds
@@ -11,5 +11,6 @@ sed -i '/ansible_host: IP_ADDRESS_OF_NODE3/a \      ansible_ssh_private_key_file
 sed -i 's/IP_ADDRESS_OF_NODE1/"192.168.33.31"/g' inventory.yml
 sed -i 's/IP_ADDRESS_OF_NODE2/"192.168.33.32"/g' inventory.yml
 sed -i 's/IP_ADDRESS_OF_NODE3/"192.168.33.33"/g' inventory.yml
-#ansible all -i inventory.yml -bv -m ping
-#./requirements_install.sh
+ansible all -i inventory.yml -bv -m ping
+./requirements_install.sh
+./deploy_and_bootstrap.sh
