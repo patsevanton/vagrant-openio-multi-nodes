e72784bb0bf2147b608fa882ff071b8c2c5282d2
Anton Patsev <apatsev@luxoft.com>
2019-08-09 13:58:50 +0600

    ansible_host: IP_ADDRESS_OF_NODE1 ansible_ssh_private_key_file

--- start_script.sh
+++ start_script.sh
@@ -5,9 +5,9 @@ git clone https://github.com/open-io/ansible-playbook-openio-deployment.git --br
 mv oiosds/products/sds .
 rm -rf oiosds
 cd sds
-sed '/      ansible_host: IP_ADDRESS_OF_NODE1/a       ansible_ssh_private_key_file: ../.vagrant/machines/node1/virtualbox/private_key' inventory.yml
-sed '/      ansible_host: IP_ADDRESS_OF_NODE2/a       ansible_ssh_private_key_file: ../.vagrant/machines/node2/virtualbox/private_key' inventory.yml
-sed '/      ansible_host: IP_ADDRESS_OF_NODE3/a       ansible_ssh_private_key_file: ../.vagrant/machines/node3/virtualbox/private_key' inventory.yml
+sed -i '/ansible_host: IP_ADDRESS_OF_NODE1/a \      ansible_ssh_private_key_file: ../.vagrant/machines/node1/virtualbox/private_key' inventory.yml
+sed -i '/ansible_host: IP_ADDRESS_OF_NODE2/a \      ansible_ssh_private_key_file: ../.vagrant/machines/node2/virtualbox/private_key' inventory.yml
+sed -i '/ansible_host: IP_ADDRESS_OF_NODE3/a \      ansible_ssh_private_key_file: ../.vagrant/machines/node3/virtualbox/private_key' inventory.yml
 sed -i 's/IP_ADDRESS_OF_NODE1/"192.168.33.31"/g' inventory.yml
 sed -i 's/IP_ADDRESS_OF_NODE2/"192.168.33.32"/g' inventory.yml
 sed -i 's/IP_ADDRESS_OF_NODE3/"192.168.33.33"/g' inventory.yml
